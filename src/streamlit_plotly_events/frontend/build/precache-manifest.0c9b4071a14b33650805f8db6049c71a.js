e6e7f75db9252f72ee8c0d27a5f8371c19ac9a07
Ceyda Cinarel <15624271+cceyda@users.noreply.github.com>
2022-06-29 05:43:10 +0000

    add built front for release

---
+++ src/streamlit_plotly_events/frontend/build/precache-manifest.0c9b4071a14b33650805f8db6049c71a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27075aebb06e710826a22517f559c460",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c661fb41e03405bb3dc",
+    "url": "./static/js/2.89a6ec8b.chunk.js"
+  },
+  {
+    "revision": "4a84cb0487aa20911c252a5e9703b6bd",
+    "url": "./static/js/2.89a6ec8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "918fa53cdc54545185bd",
+    "url": "./static/js/main.2f1f2f3e.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
