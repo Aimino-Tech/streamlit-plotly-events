feb8359c98a58981c8b87549f3bebea8389b30f8
Zafir Stojanovski <zaf.stojano@gmail.com>
2023-01-25 16:16:05 +0100

    updated `MANIFEST.in` and `setup.py`

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import setuptools
 
 setuptools.setup(
-    name="streamlit-plotly-events",
-    version="0.0.6",
-    author="Ellie Jones",
-    author_email="ellie@altaml.com",
-    description="Plotly chart component for Streamlit that also allows for events to bubble back up to Streamlit.",
-    long_description="Plotly chart component for Streamlit that also allows for events to bubble back up to Streamlit.",
+    name="streamlit-plotly-events-custom-data",
+    version="0.0.6.3",
+    author="Zafir Stojanovski",
+    author_email="zafir.stojanovski@aimino.de",
+    description="Plotly chart component for Streamlit that also allows for events to bubble back up to Streamlit. This is a fork that allows the user to specify custom_data in the plot.",
+    long_description="Plotly chart component for Streamlit that also allows for events to bubble back up to Streamlit. This is a fork that allows the user to specify custom_data in the plot.",
     long_description_content_type="text/plain",
-    url="https://github.com/null-jones/streamlit-plotly-events",
+    url="https://github.com/Aimino-Tech/streamlit-plotly-events",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    include_package_data=True,
     classifiers=[],
+    include_package_data=True,
     python_requires=">=3.6",
     install_requires=[
         "streamlit >= 0.63",
